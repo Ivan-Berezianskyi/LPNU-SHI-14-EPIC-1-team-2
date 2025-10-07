f92ce46bffda8044d5b768221271bd6e3650aadf
Ivan Berezyanskyi <gamaew.res@gmail.com>
2025-10-07 22:21:16 +0300

    Resolved conflict

--- homework.cpp
+++ homework.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main() {
     cout << "Vladyslav Niemtsov" << endl;
-    
+    cout << "Sofiia Marchynska" << endl;
+    cout << "Ivan Berezianskyi" << endl;
     return 0;
 }
 
